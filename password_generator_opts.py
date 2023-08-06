fca495530bb9b48fcf8576a9224b3fe1bb3d7723
xmondolemus <armando.lemus@xmondo.com>
2023-08-06 12:16:24 -0500

    Added argument parsing class with static method

---
+++ password_generator_opts.py
@@ -0,0 +1,74 @@
+class RandomCombo:
+    def __init__(self, quantity, lower_limit, upper_limit):
+        self.quantity = quantity
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+
+    def word_integer_combination(self):
+        import random
+        from random_word import RandomWords
+        import colorize as clr
+        blu = clr.Color('bx').set_color()
+        reset = clr.Color(None).set_color()
+        rw = RandomWords()
+        print()
+        for k in range(0, self.quantity):
+            ri_1 = random.randint(self.lower_limit, self.upper_limit)
+            ri_2 = random.randint(self.lower_limit, self.upper_limit)
+            pw = (rw.get_random_word().capitalize() + '-' + str(ri_1) + '-' + rw.get_random_word().capitalize() +
+                  '-' + str(ri_2) + '-' + rw.get_random_word().capitalize())
+            print(blu + pw + reset)
+        return print()
+
+
+class RandomString:
+    def __init__(self, quantity, length):
+        self.quantity = quantity
+        self.length = length
+
+    def random_secret(self):
+        import secrets
+        import colorize as clr
+        red = clr.Color('rx').set_color()
+        reset = clr.Color(None).set_color()
+
+        count = 0
+        while count != self.quantity:
+            charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&'
+            passwd_string = ''.join(secrets.choice(charset) for _ in range(self.length))
+            print(red + passwd_string + reset)
+            count += 1
+        return print()
+
+
+class GetInputs:
+    @staticmethod
+    def commandline_options():
+        import argparse
+        import sys
+        parser = argparse.ArgumentParser()
+        parser.add_argument("number_of_passwords", type=int, help="number of passwords")
+        parser.add_argument("lower_limit", type=int, help="lower limit")
+        parser.add_argument("upper_limit", type=int, help="upper limit")
+        parser.add_argument("random_string_length", type=int, help="random string length")
+        args = parser.parse_args()
+        number_of_passwords = args.number_of_passwords
+        lower_limit = args.lower_limit
+        upper_limit = args.upper_limit
+        random_string_length = args.random_string_length
+
+        if lower_limit > upper_limit:
+            print('lower limit must be less than upper limit')
+            sys.exit(2)
+        else:
+            return number_of_passwords, lower_limit, upper_limit, random_string_length
+
+
+if __name__ == '__main__':
+
+    try:
+        q, ll, ul, rl = GetInputs.commandline_options()
+        RandomCombo(q, ll, ul).word_integer_combination()
+        RandomString(q, rl).random_secret()
+    except Exception as err:
+        print(err)
